2d63d06f59e197ebc46fa336fd5e3784cb3f1d24
IIChibi0ChanII <mahamudashanta@gmail.com>
2025-06-01 15:05:19 +0600

    finish

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ for ro in range(1, num+1):
 l = num
 
 for row in range(1, num+1):
-    for col in range(num,l-num,-1):
+    for col in range(num+1,row,-1):
         l += 1
         print("*", end=" ")
     print()
